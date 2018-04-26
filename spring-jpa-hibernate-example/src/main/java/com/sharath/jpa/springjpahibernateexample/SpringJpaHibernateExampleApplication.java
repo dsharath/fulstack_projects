950e07b487d5c4071e7d5014e58cba2120a6c7f1
sharath chandra <dsharathc7@gmail.com>
2018-04-25 21:47:55 -0500

    doc: Adding spring jpa using hibernate

---
+++ spring-jpa-hibernate-example/src/main/java/com/sharath/jpa/springjpahibernateexample/SpringJpaHibernateExampleApplication.java
@@ -0,0 +1,12 @@
+package com.sharath.jpa.springjpahibernateexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaHibernateExampleApplication.class, args);
+	}
+}
