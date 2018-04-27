4fbabc013a33fa9a393248c5ad547409030f18e7
sharath chandra <dsharathc7@gmail.com>
2018-04-26 21:57:39 -0500

    doc: Adding basic springboot application

---
+++ springboot.demo/src/main/java/springboot/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+/*package springboot.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(DemoApplication.class, args);
+
+	}
+	
+	@Value(value = "${spring.datasource.url}")
+	String dbName;
+	
+	//@Autowired
+	@Override
+	
+	public void run(String...strings arg0) throw exception{
+		System.out.println("************"+dbName+"************");
+	}
+}
+*/
